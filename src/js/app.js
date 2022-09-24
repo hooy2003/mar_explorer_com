49488989102babd054e69e1fa791331efedc5428
Tony Mac <hooycp@gmail.com>
2022-09-24 16:25:23 +0800

    Add index change

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-console.log('1')
+(function(){
+  var burger = document.querySelector('.__burger__container'),
+      header = document.querySelector('.__mobile__header');
+  burger.addEventListener("click", function() {
+    header.classList.toggle('__menu__opened');
+  });
+}());
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': {'max': '480px'},
+        'md': {'max': '640px'},
+        'xl': {'max': '1000px'},
+      },
       height: {
         '1px': '1px',
         '2px': '2px'
@@ -21,23 +26,28 @@ module.exports = {
       color: {
         'explorer-bule': '#145D9F',
         'explorer-d-bule': '#1628D1',
-        'explorer-white': '#F6F6F6'
+        'explorer-white': '#145D9F',
+        'explorer-light-white': '#F6F6F6'
       },
       backgroundColor:  {
         'explorer-bule': '#145D9F',
         'explorer-d-bule': '#1628D1',
-        'explorer-white': '#F6F6F6'
+        'explorer-white': '#145D9F',
+        'explorer-light-white': '#F6F6F6'
       },
       textColor:  {
         'explorer-bule': '#145D9F',
         'explorer-d-bule': '#1628D1',
-        'explorer-white': '#F6F6F6'
+        'explorer-white': '#145D9F',
+        'explorer-light-white': '#F6F6F6'
       },
       backgroundImage: {
         's1-bg': "url('/img/s1_bg.png')",
         's2-bg': "url('/img/s2_bg.png')",
         's2-wrap': "url('/img/s2_wrap.png')",
+        's2-wrap-moblie': "url('/img/s2_wrap_moblie.png')",
         's4-left': "url('/img/s4_left.png')",
+        's4-left-mobile': "url('/img/s4_left_mobile.png')",
       },
       flex: {
         '100': '1 1 100%'

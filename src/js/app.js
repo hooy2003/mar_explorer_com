8c65684bcf1218d0ff9c6fb2609b09979e06552a
Tony Mac <hooycp@gmail.com>
2022-09-29 21:25:10 +0800

    Add youtube video

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,54 @@
   burger.addEventListener("click", function() {
     header.classList.toggle('__menu__opened');
   });
+  if(document.getElementById('autoplay_1')&&document.getElementById('autoplay_1').clientHeight>0) {
+    document.getElementById('autoplay_1').play();
+  }
+  if(document.getElementById('autoplay_2')&&document.getElementById('autoplay_2').clientHeight>0) {
+    document.getElementById('autoplay_2').play();
+  }
 }());
+
+
+// Get element
+var youtubeEmbedElement = document.getElementById("youtubeEmbed");
+if(youtubeEmbedElement) {
+  // Add YouTube API script
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  var videoId = youtubeEmbedElement.dataset.videoId;
+
+  onYouTubeIframeAPIReady = function () {
+    player = new YT.Player("youtubeEmbed", {
+      videoId: videoId, // YouTube Video ID
+      playerVars: {
+        autoplay: 1, // Auto-play the video on load
+        autohide: 1, // Hide video controls when playing
+        disablekb: 1,
+        controls: 0, // Hide pause/play buttons in player
+        showinfo: 0, // Hide the video title
+        modestbranding: 1, // Hide the Youtube Logo
+        loop: 1, // Run the video in a loop
+        fs: 0, // Hide the full screen button
+        rel: 0,
+        enablejsapi: 1
+      },
+      events: {
+        onReady: function (e) {
+          e.target.mute();
+          e.target.playVideo();
+        },
+        onStateChange: function (e) {
+          if (e.data === YT.PlayerState.PLAYING) {
+            document.getElementById("youtubeEmbed").classList.add("loaded");
+          }
+        }
+      }
+    });
+  };
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -48,12 +48,12 @@ module.exports = {
         'explorer-light-white': '#F6F6F6'
       },
       backgroundImage: {
-        's1-bg': "url('/img/s1_bg.png')",
-        's2-bg': "url('/img/s2_bg.png')",
-        's2-wrap': "url('/img/s2_wrap.png')",
-        's2-wrap-moblie': "url('/img/s2_wrap_moblie.png')",
-        's4-left': "url('/img/s4_left.png')",
-        's4-left-mobile': "url('/img/s4_left_mobile.png')",
+        's1-bg': "url('../img/s1_bg.png')",
+        's2-bg': "url('../img/s2_bg.png')",
+        's2-wrap': "url('../img/s2_wrap.png')",
+        's2-wrap-moblie': "url('../img/s2_wrap_moblie.png')",
+        's4-left': "url('../img/s4_left.png')",
+        's4-left-mobile': "url('../img/s4_left_mobile.png')",
       },
       flex: {
         '100': '1 1 100%'
